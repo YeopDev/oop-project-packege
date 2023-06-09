bd9e9277586cdcc8f8282f29fa50d0edf7273b67
sangyeop <yeop1.dev@gmail.com>
2023-06-10 00:18:24 +0900

    :recycle: refactor(Turn): 볼링 라운드 점수규칙개선

--- src/main/java/bollingGame/turn/Turn.java
+++ src/main/java/bollingGame/turn/Turn.java
@@ -4,64 +4,98 @@ import bollingGame.bolling.GamePlayer;
 
 public class Turn {
     private GamePlayer player;
+    private int score;
     private boolean first;
 
     public Turn(GamePlayer player) {
         this.player = player;
+        this.score = 0;
         this.first = true;
     }
 
-    public void play() {
-        int roll = player.rollBall();
-        boolean isStrike = player.strike();
-        boolean isSpare = player.spare();
+    public void play() { // 1 ~ 9 라운드까지는 play사용
+        boolean isStrike = player.strike();// 이전에 스트라이크를 쳤는지
+        boolean isSpare = player.spare();// 이전에 스페어를 처리했는지
+        int pins = 10; //원래 핀 개수
+
         for (int i = 0; i < 2; i++) {
-            int pins = 10;
-            if (first) {
+            if (first) {// 첫 번째 굴림
+                int roll = player.rollBall();
                 pins -= roll;
-                // 첫 번째 굴림
                 if (isStrike) {
-                    if (isStrikePreviousRound(pins)) {
+                    if (isZeroPin(pins)) {
                         player.changeStrike(true);
+                        player.calculate(roll);
                         break;
                     } else {
                         player.changeStrike(false);
-                        player.calculateMultiply(2);
-                        player.calculate(10 + player.score() * player.multiply());
+                        this.score += roll;
                     }
+
+                } else if (isSpare) {
+                    if (isZeroPin(pins)) {
+                        player.changeStrike(true);
+                        player.calculate(roll * 2);
+                        break;
+                    } else {
+                        player.changeStrike(false);
+                        player.calculate(roll * 2);
+                    }
+
                 } else {
-                    player.calculate(roll);
-                    player.calculateMultiply(0);
+                    if (isZeroPin(pins)) {
+                        player.changeStrike(true);
+                        player.calculate(roll);
+                        break;
+                    } else {
+                        player.changeStrike(false);
+                        player.calculate(roll);
+                    }
                 }
 
                 first = false;
-            } else {
-                // 두번째 굴림
+            } else {// 두번째 굴림
+                int roll = 0;
+                while (true) {
+                    roll = player.rollBall();
+                    if (pins >= roll) {
+                        break;
+                    }
+                }
                 pins -= roll;
-                if (isSpare) {
-                    if (isSparePreviousRound(pins)) {
+                if (isStrike) {
+                    if (isZeroPin(pins)) {
                         player.changeSpare(true);
+                        player.calculate((roll + this.score) * 2);
                     } else {
                         player.changeSpare(false);
-                        player.calculateMultiply(2);
-                        player.calculate(pins * player.multiply());
+                        player.calculate((roll + this.score) * 2);
                     }
+
+                } else if (isSpare) {
+                    if (isZeroPin(pins)) {
+                        player.changeSpare(true);
+                        player.calculate(roll);
+                    } else {
+                        player.changeSpare(false);
+                        player.calculate(roll);
+                    }
+
                 } else {
-                    player.calculate(roll);
-                    player.calculateMultiply(0);
+                    if (isZeroPin(pins)) {
+                        player.changeSpare(true);
+                        player.calculate(roll);
+                    } else {
+                        player.changeSpare(false);
+                        player.calculate(roll);
+                    }
+
                 }
             }
         }
     }
 
-    private boolean isStrikePreviousRound(int pins) {
-        if (pins == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isSparePreviousRound(int pins) {
+    private boolean isZeroPin(int pins) {
         if (pins == 0) {
             return true;
         }
