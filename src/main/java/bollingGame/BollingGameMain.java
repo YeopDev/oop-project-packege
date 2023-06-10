31d098139f9d97543f11b8b64eb4cd5e97852bad
sangyeop <yeop1.dev@gmail.com>
2023-06-11 01:11:50 +0900

    :recycle: refactor(BollingGameMain): 볼링게임 메인 :recycle: refactor(README): 요구사항 수정

--- src/main/java/bollingGame/BollingGameMain.java
+++ src/main/java/bollingGame/BollingGameMain.java
@@ -1,34 +1,29 @@
 package bollingGame;
 
-import bollingGame.bolling.GamePlayer;
-import bollingGame.bolling.RandomRollBall;
-import bollingGame.bolling.RandomRollBallImpl;
-import bollingGame.bolling.computer.Computer;
-import bollingGame.bolling.player.Player;
-import bollingGame.turn.Turn;
-
 public class BollingGameMain {
     public static void main(String[] args) {
         RandomRollBall randomRollBall = new RandomRollBallImpl();
-        GamePlayer player = new Player("human1", randomRollBall);
-        GamePlayer computer = new Computer("CP", randomRollBall);
 
-        for (int i = 0; i < 10; i++) {
-            Turn playerTurn = new Turn(player);
-            Turn computerTurn = new Turn(computer);
-            playerTurn.play();
-            computerTurn.play();
+        GameManagement gameManagement = new GameManagement(randomRollBall);
+        boolean firstRoll = true;
+
+        for (int frame = 1; frame <= 10; frame++) {
+            for (int two = 0; two < 2; two++) {
+                if (firstRoll) {
+                    gameManagement.roll();
+                    firstRoll = false;
+                } else {
+                    gameManagement.roll();
+                    firstRoll = true;
+                }
+            }
+
+            if (frame == 10 && (gameManagement.getLastFrame().isSpare() || gameManagement.getLastFrame().isStrike())) {
+                gameManagement.roll();
+            }
         }
 
-        System.out.println("Player's score: " + player.score());
-        System.out.println("Computer's score: " + computer.score());
-
-        if (player.score() < computer.score()) {
-            System.out.println("컴퓨터의 승리 - 점수차: " + (computer.score() - player.score()));
-        } else if (player.score() > computer.score()) {
-            System.out.println("플레이어의 승리 - 점수차: " + (player.score() - computer.score()));
-        } else {
-            System.out.println("무승부");
-        }
+        int score = gameManagement.getScore();
+        System.out.println("최종 점수: " + score);
     }
 }
