72732cf70ea18e63b7687d203e119e658effdf26
sangyeop <yeop1.dev@gmail.com>
2023-06-11 01:28:40 +0900

    :white_check_mark: test(GameManagement): 게임관리자 테스트

--- src/main/java/bollingGame/GameManagement.java
+++ src/main/java/bollingGame/GameManagement.java
@@ -28,7 +28,7 @@ public class GameManagement {
         Frame currentFrame = getCurrentFrame();
 
         if (currentFrame == null || currentFrame.getSecondRoll() == -1) {
-            currentFrame = new Frame(pins, -1);
+            currentFrame = new Frame(pins, 0);
             frames.add(currentFrame);
             return;
         }
