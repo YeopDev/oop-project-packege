217155c4eceec624cc72fb4a120486f2274b3e48
sangyeop <yeop1.dev@gmail.com>
2023-06-12 01:19:35 +0900

    :recycle: refactor(BookStockQuantity): MAGIC NUMBER 적용

--- src/main/java/bookManagement/Library/Book/BookStockQuantity.java
+++ src/main/java/bookManagement/Library/Book/BookStockQuantity.java
@@ -3,18 +3,19 @@ package bookManagement.Library.Book;
 import static java.util.Objects.isNull;
 
 public record BookStockQuantity(Book book, int stockQuantity) {
+    private static final int MINIMUM_QUANTITY = 0;
 
     public BookStockQuantity {
         if (isNull(book)) {
             throw new IllegalArgumentException("도서정보가 없습니다.");
         }
-        if (stockQuantity < 0) {
-            throw new IllegalArgumentException("재고수량에 올바르지 않은 값이 할당 되었습니다.");
+        if (stockQuantity < MINIMUM_QUANTITY) {
+            throw new IllegalArgumentException("재고수량은 %d 보다 작을 수 없습니다.".formatted(MINIMUM_QUANTITY));
         }
     }
 
     public Long returnBookId() {
-        return book.responseId();
+        return book.id();
     }
 
     public BookStockQuantity decreaseStockQuantity() {
